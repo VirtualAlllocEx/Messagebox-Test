4c5c6b73630ddce04df8d1ae40ecb002f8bc744e
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2022-08-22 14:36:11 +0200

    Create messagebox.ps1

---
+++ messagebox.ps1
@@ -0,0 +1,3 @@
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+ 
+[System.Windows.Forms.MessageBox]::Show("Congratulations, it looks like, your product do not in general block downloading and executing harmless code from pastebin via Invoke-Expression in PowerShell","Dein Titel",0)
